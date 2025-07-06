e58c71eb8d69004a17297f509eae33d445b2a86a
shiraayuki <wegerernikolas@gmail.com>
2025-07-06 19:13:04 +0200

    Add thought process comments

--- problems/217_contains_duplicate/main.c
+++ problems/217_contains_duplicate/main.c
@@ -5,8 +5,12 @@
 int cmp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
 
 bool containsDuplicate(int *nums, int numsSize) {
+  // First i sort the array using quick sort. With this state its much easier
+  // to check the duplicates because now they would be next to each other.
   qsort(nums, numsSize, sizeof(int), cmp);
 
+  // Now we can just iterate over the array and check if the integer next
+  // to it is equal to itself.
   for (int i = 1; i < numsSize; i++) {
     if (nums[i] == nums[i - 1]) {
       return true;
--- problems/242_valid_anagram/main.c
+++ problems/242_valid_anagram/main.c
@@ -6,13 +6,20 @@
 int cmp(const void *a, const void *b) { return *(char *)a - *(char *)b; }
 
 bool isAnagram(char *s, char *t) {
+  // Because i use the size of the 'strings' later again, its smarter
+  // to store them to minmize the callbacks.
   size_t sizeS = strlen(s);
   size_t sizeT = strlen(t);
 
+  // If the sizes are different there is no way to create an anagram,
+  // so we can already exit here.
   if (sizeS != sizeT) {
     return false;
   }
 
+  // If we sort both arrays, we can simply iterate over one of them once
+  // and compare each loaction with the other array. In case they differ
+  // we know it cant be an anagram so we return false.
   qsort(s, sizeS, sizeof(char), cmp);
   qsort(t, sizeT, sizeof(char), cmp);
 
